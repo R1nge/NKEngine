8ae2f2b8b1661810ab76049e2d275a4da39c0207
R1nge <r1ngegamedev@gmail.com>
2025-10-18 14:36:53 +0300

    Display image (need to copy assets to the executable folder)

--- main.cpp
+++ main.cpp
@@ -1,55 +1,117 @@
-#include <iostream>
 #include <SDL.h>
 #include <stdio.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    //TODO: draw a box to a window
-    //TODO: move the box
-    //TODO: move the camera
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
-    //Screen dimension constants
-    const int SCREEN_WIDTH = 640;
-    const int SCREEN_HEIGHT = 480;
+//Starts up SDL and creates window
+bool init();
 
-    SDL_Window *window = NULL;
+//Loads media
+bool loadMedia();
 
-    //The surface contained by the window
-    SDL_Surface *screenSurface = NULL;
+//Frees media and shuts down SDL
+void close();
 
-    //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    }
-    else
-    {
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        }else {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
 
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
 
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface( gWindow );
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load splash image
+	gHelloWorld = SDL_LoadBMP( "assets/bocchi.bmp" );
+	if( gHelloWorld == NULL )
+	{
+		printf( "Unable to load image %s! SDL Error: %s\n", "assets/bochi.bmp", SDL_GetError() );
+		success = false;
+	}
+
+	return success;
+}
+
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface( gHelloWorld );
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+int main()
+{
+	//Start up SDL and create window
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		//Load media
+		if( !loadMedia() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+		{
+			//Apply the image
+			SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
+
+			//Update the surface
+			SDL_UpdateWindowSurface( gWindow );
 
             //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ) {
-                while( SDL_PollEvent( &e ) ) {
-                    if( e.type == SDL_QUIT ) quit = true;
-                }
-            }
-        }
-    }
-        //Destroy window
-    SDL_DestroyWindow( window );
+            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+		}
+	}
 
-        //Quit SDL subsystems
-    SDL_Quit();
+	//Free resources and close SDL
+	close();
 
-    return 0;
+	return 0;
 }
