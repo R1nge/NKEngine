37e1033370e1fd306a7ddcb910c576c6329f0c57
R1nge <r1ngegamedev@gmail.com>
2025-10-19 22:41:41 +0300

    Engine Vector2Int

---
+++ NKVector2Int.cpp
@@ -0,0 +1,5 @@
+//
+// Created by r1nge on 10/19/25.
+//
+
+#include "NKVector2Int.h"
---
+++ NKVector2Int.h
@@ -0,0 +1,15 @@
+//
+// Created by r1nge on 10/19/25.
+//
+
+#ifndef NKENGINE_NKVECTOR2INT_H
+#define NKENGINE_NKVECTOR2INT_H
+
+
+struct  NKVector2Int {
+    int X;
+    int Y;
+};
+
+
+#endif //NKENGINE_NKVECTOR2INT_H
