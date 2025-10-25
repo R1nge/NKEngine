2848cd27997700313d0c39c53936b7f6fd3e8dee
R1nge <r1ngegamedev@gmail.com>
2025-10-25 21:20:06 +0300

    Todos

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
-#include <SDL2/SDL.h>
-#include <cstdio>
-#include <SDL2/SDL_image.h>
 #include <string>
 
 #include "MyGameEventSubscriber.h"
 #include "NKEngine.h"
 
-//TODO: create a separate window class, expose via engine
+//TODO: engine generate id, store in a map by id, e.x renderer has a map of sprites, physics engine has a map of colliders, they are synced by id
+//TODO: so, when engine destroys an object it destroys if everywhere it exists and releases resources
+//TODO: engine -> renderer (sprite)
+//TODO: engine -> phys engine (col)
+//TODO: engine -> transform
+//TODO: stored by the same id
+
 //TODO: save engine settings into ini file https://github.com/dujingning/inicpp    (resolution, reference resolution, scale (width-height 0-1)
 
 //TODO: store object in dict<uuid, NKObject>, destroy by id
@@ -25,7 +28,7 @@ int main() {
 
     nk_engine->CreateSprite("assets/space_invaders.png",new NKSpriteData(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20 * SCALE_X, 10 * SCALE_Y, 0, 0, 20, 10, 255, 255, 255));
     auto player = nk_engine->CreateSprite("assets/space_invaders.png",new NKSpriteData(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20 * SCALE_X, 10 * SCALE_Y, 0, 48, 20, 10, 255, 255,255));
-    //Main loop
+
     NKEventSubscriber *mySub = new MyGameEventSubscriber(player, nk_engine);
     nk_engine->EventDispatcher->AddSubscriber(mySub);
 
