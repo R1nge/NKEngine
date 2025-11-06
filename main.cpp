9bb93efa7ccc969ec407424e394350f0affc7849
R1nge <r1ngegamedev@gmail.com>
2025-11-06 14:57:55 +0300

    Engine box2D integration

--- main.cpp
+++ main.cpp
@@ -106,20 +106,35 @@ int main() {
 
     b2::World w(b2::World::Params{});
 
-    b2::Body::Params bp;
-    bp.type = b2_dynamicBody;
+    b2::Body::Params rigidBodyParent;
+    rigidBodyParent.type = b2_dynamicBody;
 
-    b2::Body b = w.CreateBody(b2::OwningHandle, bp);
+    b2::Body rigidBody = w.CreateBody(b2::OwningHandle, rigidBodyParent);
 
-    b.CreateShape(
+
+    rigidBody.CreateShape(
         b2::DestroyWithParent,
         b2::Shape::Params{},
         b2Circle{.center = b2Vec2(), .radius = 3}
     );
 
-    for (int i = 0; i < 10; i++) {
+    rigidBody.ApplyForceToCenter(b2Vec2(0, 0), true);
+
+    b2::Body::Params staticParent;
+    staticParent.type = b2_staticBody;
+    staticParent.position = b2Vec2(0, -2);
+
+    b2::Body staticBody = w.CreateBody(b2::OwningHandle, staticParent);
+
+    staticBody.CreateShape(
+        b2::DestroyWithParent,
+        b2::Shape::Params{},
+        b2MakeBox(100, 1)
+    );
+
+    for (int i = 0; i < 500; i++) {
         w.Step(1 / 60.f, 4);
-        std::cout << b.GetPosition().y << "\n";
+        std::cout << rigidBody.GetPosition().y << "\n";
     }
 
     while (!nk_engine->Quitting()) {
