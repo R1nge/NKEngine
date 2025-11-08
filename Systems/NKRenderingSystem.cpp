2bc5737222d469e6e0867ff9e0b5ce89d5f46d22
R1nge <r1ngegamedev@gmail.com>
2025-11-08 22:58:03 +0300

    Engine Dear ImGui integration: remove component and system word part

--- Systems/NKRenderingSystem.cpp
+++ Systems/NKRenderingSystem.cpp
@@ -100,7 +100,7 @@ void SystemsInspector(bool my_tool_active, NKEngine *engine) {
         // Display the systems
         for (const auto &pair: systemMap) {
             std::string typeName = typeid(*pair.second.get()).name();
-            typeName = typeName.substr(0, typeName.size()-6);
+            typeName = typeName.substr(0, typeName.size() - 6);
 
             size_t startPos = 0;
             while (startPos < typeName.length() && std::isdigit(typeName[startPos])) {
